ef09dc4f7dbe0442a90e17d9ef40685fd76c58c5
fonzi <48169018+alfonsog714@users.noreply.github.com>
2025-09-08 14:30:48 -0700

    event system & dynamic array stuff started

---
+++ engine/src/containers/darray.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "defines.h"
+
+/**
+ * Memory layout
+ * u64 capacity = number of elements that can be held
+ * u64 length = number of elements currently held
+ * u64 stride = the size of each element in bytes
+ * void *elements = the array of elements itself
+ */
+
+enum
+{
+    DARRAY_CAPACITY,
+    DARRAY_LENGTH,
+    DARRAY_STRIDE,
+    DARRAY_FIELD_LENGTH
+};
+
+RCAPI void *_darray_create(u64 length, u64 stride);
+RCAPI void _darray_destory(void *array);
+
+RCAPI u64 _darray_field_get(void *array, u64 field);
+RCAPI void _darray_field_set(void *array, u64 field, u64 value);
+
+RCAPI void *_darray_resize(void *array);
+
+RCAPI void *_darrray_push(void *array, const void *value_ptr);
+RCAPI void _darray_pop(void *array, void *dest);
+
+RCAPI void *_darray_pop_at(void *array, u64 index, void *dest);
+RCAPI void *_darray_insert_at(void *array, u64 index, void *value_ptr);
+
+#define DARRAY_DEFAULT_CAPACITY 1
+#define DARRAY_RESIZE_FACTOR 2
+
+#define darray_create(type) \
+    _darray_create(DARRAY_DEFAULT_CAPACITY, sizeof(type))
+
+#define darray_reserve(type, capacity) \
+    _darray_create(capacity, sizeof(type))
+
+#define darray_destory(array) _darray_destory(array);
+
+#define darray_push(array, value)          \
+    {                                      \
+        typeof(value) temp = value;        \
+        array = _darray_push(array, &temp) \
+    }
+
+#define darray_pop(array, value_ptr) \
+    _darray_pop(array, value_ptr)
+
+#define darray_insert_at(array, index, value)           \
+    {                                                   \
+        typeof(value) temp = value;                     \
+        array = _darray_insert_at(array, index, &temp); \
+    }
+
+#define darray_pop_at(array, index, value_ptr) \
+    _darray_pop_at(array, index, value_ptr)
+
+// CONTINUE HERE: 008: Events @31:45
---
+++ engine/src/core/event.h
@@ -0,0 +1,112 @@
+#pragma once
+
+#include "defines.h"
+
+typedef struct event_context
+{
+    union
+    {
+        i64 i64[2];
+        u64 u64[2];
+        f64 f64[2];
+
+        i32 i32[4];
+        u32 u32[4];
+        f32 f32[4];
+
+        i16 i16[8];
+        u16 u16[8];
+
+        i8 i8[16];
+        u8 u8[16];
+
+        char c[16];
+    } data;
+} event_context;
+
+// Should return TRUE if handled.
+typedef b8 (*PFN_on_event)(u16 code, void *sender, void *listener_inst, event_context data);
+
+b8 event_initialize();
+void event_shutdown();
+
+/**
+ * Registers a listener for when events are sent with the provided code.
+ * @param code The event code to listen for.
+ * @param listener A pointer to a listener instance. Can be 0 / NULL.
+ * @param on_event The callback function pointer to be invoked when the event code is fired.
+ * @returns TRUE if the vent is successfully registered; otherwise it returns FALSE.
+ */
+RCAPI b8 event_register(u16 code, void *listener, PFN_on_event on_event);
+
+/**
+ * Unregister from listening to events with provided code.
+ * @param code The event code to stop listening for.
+ * @param listener Pointer to a listener instance. Can be NULL / 0.
+ * @param on_event Callback pointer to be unregistered.
+ * @returns TRUE if event is successfully unregistered. FALSE otherwise.
+ */
+RCAPI b8 event_unregister(u16 code, void *listener, PFN_on_event on_event);
+
+/**
+ * Fires an event to listeners of the provided code.
+ * @param code The event code to fire
+ * @param sender A pointer to the sender. Can be NULL.
+ * @param data The event data.
+ * @returns TRUE if handled, FALSE otherwise.
+ */
+RCAPI b8 event_fire(u16 code, void *sender, event_context context);
+
+// System internal event codes. Application should use codes beyond 255
+typedef enum system_event_code
+{
+    /* Shuts down the application on the next frame.*/
+    EVENT_CODE_APPLICATION_QUIT = 0x01,
+
+    /**
+     * Context usage:
+     * u16 key_code = data.data.u16[0]
+     */
+    EVENT_CODE_KEY_PRESSED = 0x02,
+
+    /**
+     * Context usage:
+     * u16 key_code = data.data.u16[0]
+     */
+    EVENT_CODE_KEY_RELEASED = 0x03,
+
+    /**
+     * Context usage:
+     * u16 button = data.data.u16[0]
+     */
+    EVENT_CODE_BUTTON_PRESSED = 0x04,
+
+    /**
+     * Context usage:
+     * u16 button = data.data.u16[0]
+     */
+    EVENT_CODE_BUTTON_RELEASED = 0x05,
+
+    /**
+     * Context usage:
+     * u16 x = data.data.u16[0]
+     * u16 y = data.data.u16[1]
+     */
+
+    EVENT_CODE_MOUSE_MOVED = 0x06,
+
+    /**
+     * Context usage:
+     * u8 z_delta = data.data.u8[0]
+     */
+    EVENT_CODE_MOUSE_WHEEL = 0x07,
+
+    /**
+     * Context usage:
+     * u16 width = data.data.u16[0]
+     * u16 height = data.data.u16[1]
+     */
+    EVENT_CODE_RESIZED = 0x08,
+
+    MAX_EVENT_CODE = 0xFF
+} system_event_code;
